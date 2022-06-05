5f506495eb5310cca197ea2fef657580db027bdb
Frank-197 <frankomosh197@gmail.com>
2022-06-05 20:55:28 +0300

    Done.

---
+++ index.js
@@ -0,0 +1,13 @@
+function introduction (name='Aki') {
+    return `Hi, my name is ${name}.`;
+}
+function introductionWithLanguage(name="Gracie",language="Python"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+}
+function sayHelloTo(firstName){
+    return `Hello, ${firstName}`
+}
+function introductionWithLanguageOptional(name="Gracie", language="JavaScript"){
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`;
+
+}
